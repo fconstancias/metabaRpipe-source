593fd97f0ae6b8ca51c12900cdb4f29fffd570e8
Florentin Constancias <florentinconstancias@gmail.com>
2022-10-04 09:50:30 +0200

    Update functions.R
    should be alright, removed the "trim" when cutadapt is atropos...

--- Rscripts/functions.R
+++ Rscripts/functions.R
@@ -238,13 +238,14 @@ run_atropos <- function(raw_files_path,
     {
       
       for(i in seq_along(fnFs)) {
-        system2(atropos, args = c("trim", "--pair-filter any",
-                                  "--no-indels", "--discard-untrimmed", "--max-n 0", " -j ", NSLOTS,
+        system2(atropos, args = c( "--pair-filter any",
+                                  "--no-indels", "--discard-untrimmed", "--max-n 0", "-j ", NSLOTS,
                                   paste("-g", PRIMER_F) , paste("-G", PRIMER_R), "-n", 2, #paste("-g", PRIMER_F, "-a", dada2:::rc(PRIMER_R)) , paste("-G", PRIMER_R, "-A", dada2:::rc(PRIMER_F)), "-n", 4,
                                   # "-O", MIN_F %>% round(0), #primer match is >= 2/3 of primer length, from Fred Mahé's swarm pipeline
                                   "-o", fnFs_cut[i], "-p", fnRs_cut[i], # output files
-                                  fnFs[i],  fnRs[i],
-                                  "--minimum-length ", MIN_L)) # input files
+                                  "--minimum-length ", MIN_L,
+                                  fnFs[i],  fnRs[i] # input file
+                                )) # input files
       }
     }
   }
